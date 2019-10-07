37946cd61726a3d558b96d315941cbc4bb2c2b29
Nikita Barinov <nikitabarinov2301@gmail.com>
2019-10-08 00:22:01 +0400

    change PidorDay Component structur

--- src/components/PidorDay/PidorDay.jsx
+++ src/components/PidorDay/PidorDay.jsx
@@ -1,42 +1,38 @@
 import React from 'react';
-import { string, number, shape, func } from 'prop-types';
+import { string, number, shape } from 'prop-types';
 import classNames from 'classnames';
 
 import './PidorDay.css';
 
 import Button from 'components/Button';
 
-const PidorDay = ({ className, user, onClick, ...restProps }) => {
+import { DONATE_LINK } from 'constants/vk';
+
+const PidorDay = ({ className, user, ...restProps }) => {
     const name = `${user.first_name} ${user.last_name}`;
 
-    function handleButtonClick(e) {
-        e.preventDefault();
-        e.stopPropagation();
-
-        if (onClick) {
-            onClick();
-        }
-    }
-
     return (
-        <a
-            className={classNames(className, 'PidorDay')}
-            href={`https://vk.com/id${user.vk_user_id}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            {...restProps}>
-            <div className="PidorDay__avatar">
-                <img src={user.avatar_200} alt={name} />
-            </div>
-            <h2 className="PidorDay__name" children={name} />
-            <p className="PidorDay__status" children="Пидор дня" />
+        <div className={classNames(className, 'PidorDay')} {...restProps}>
+            <a
+                className="PidorDay__link"
+                href={`https://vk.com/id${user.vk_user_id}`}
+                target="_blank"
+                rel="noopener noreferrer">
+                <div className="PidorDay__avatar">
+                    <img src={user.avatar_200} alt={name} />
+                </div>
+                <h2 className="PidorDay__name" children={name} />
+                <p className="PidorDay__status" children="Пидор дня" />
+            </a>
 
             <Button
+                href={DONATE_LINK}
+                target="_blank"
+                rel="noopener noreferrer"
                 size="medium"
                 full
-                children="Стать пидором дня"
-                onClick={handleButtonClick} />
-        </a>
+                children="Стать пидором дня" />
+        </div>
     );
 };
 
@@ -45,8 +41,7 @@ PidorDay.propTypes = {
     user: shape({
         vk_user_id: number,
         avatar_200: string,
-    }),
-    onClick: func
+    })
 };
 
 export default PidorDay;
